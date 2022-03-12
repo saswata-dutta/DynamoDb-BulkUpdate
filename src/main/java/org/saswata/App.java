5f90a054d2eea089a43620802cab372571058cbd
saswata <saswat.dutta@gmail.com>
2022-03-12 19:16:18 +0530

    unifies completiion and ex handlers

--- src/main/java/org/saswata/App.java
+++ src/main/java/org/saswata/App.java
@@ -1,73 +1,115 @@
 package org.saswata;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) {
-        IntStream.range(0, 5).forEach(id ->
-                checkPointExists("", id)
-                        .thenCompose(exists ->
-                                exists ?
-                                        noop() :
-                                        batchUpdate(null, 3))
-                        .thenCompose(nil -> checkPointUpdate("", id))
+        final Semaphore permits = new Semaphore(3);
+        final List<Integer> failures = Collections.synchronizedList(new ArrayList<>());
+
+        IntStream.range(0, 7).forEach(id ->
+                processAsync("", id, null, permits, failures)
         );
 
-        sleep(11);
+        sleep(6);
+        synchronized (failures) {
+            log("Failures : " + failures);
+        }
     }
 
-    private static final boolean INSIDE_BOOM0 = false;
-    private static final boolean INSIDE_BOOM1 = false;
-    private static final boolean INSIDE_BOOM2 = false;
-    private static final boolean INSIDE_BOOM3 = false;
-
-    private static CompletableFuture<Void> noop() {
-        return CompletableFuture.completedFuture(null);
+    private static CompletableFuture<Boolean> noop() {
+        return CompletableFuture.completedFuture(false);
     }
 
-    private static void log(String message) {
+    private static CompletableFuture<Boolean> done() {
+        return CompletableFuture.completedFuture(true);
+    }
+
+    private synchronized static void log(final String message) {
         System.out.println("[" + Thread.currentThread().getName() + "]\t" + LocalTime.now() + " -> " + message);
     }
 
-    private static CompletableFuture<Boolean> checkPointExists(String fileId, int val) {
-        return CompletableFuture
-                .supplyAsync(() -> {
-                    sleep(2);
-                    if (INSIDE_BOOM0) throw new RuntimeException("Failed checkPointExists " + fileId + val);
-                    log("checkPointExists " + val);
-                    return false;
+    private synchronized static void log(final Throwable e) {
+        System.err.println("[" + Thread.currentThread().getName() + "]\t" + LocalTime.now() + " -> " + e.getMessage());
+    }
+
+    private static void processAsync(final String fileId, final int val, final String[] lines,
+                                     final Semaphore permits,
+                                     final List<Integer> failures) {
+        checkPointFind("", val)
+                .thenCompose(found ->
+                        batchUpdate(found, lines, val, 3))
+                .thenCompose(updated -> checkPointUpdate(updated, "", val))
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        log(ex);
+                        failures.add(val);
+                    } else {
+                        log("Work Done = " + result + " id : " + val);
+                    }
                 });
     }
 
-    private static CompletableFuture<Void> batchUpdate(String[] lines, int retriesRemaining) {
+    private static CompletableFuture<Boolean> checkPointFind(final String fileId, final int val) {
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    log("Started id : " + val);
+                    sleep(1);
+                    if (val == 1)
+                        throw new RuntimeException("Failed checkPointExists id : " + val);
+                    boolean exists = val == 0;
+                    log("checkPointExists " + exists + " id : " + val);
+                    return exists;
+                });
+    }
+
+    private static CompletableFuture<Boolean> batchUpdate(final boolean checkPointFound,
+                                                          final String[] lines, final int val,
+                                                          final int retriesRemaining) {
         if (retriesRemaining < 1) {
-            throw new RuntimeException("Retries Exhausted");
+            throw new RuntimeException("Retries Exhausted id : " + val);
+        }
+
+        if (checkPointFound) {
+            log("batchUpdate skipped id : " + val);
+            return noop();
         }
 
         return CompletableFuture
                 .supplyAsync(() -> {
-                    sleep(2);
-                    log("batchUpdate attempt id : " + retriesRemaining);
-                    if (INSIDE_BOOM1) throw new RuntimeException("Failed batchUpdate");
+                    sleep(1);
+                    log("batchUpdate retriesRemaining " + retriesRemaining + " id : " + val);
+                    if (val == 2) throw new RuntimeException("Failed batchUpdate id : " + val);
                     return lines;
-                }).thenCompose(x -> !INSIDE_BOOM3 && retriesRemaining == 1 ?
-                        noop() :
-                        batchUpdate(x, retriesRemaining - 1));
+                }).thenCompose(x -> retriesRemaining > 1 || val == 3 ?
+                        batchUpdate(false, x, val, retriesRemaining - 1) : done()
+                );
     }
 
-    private static CompletableFuture<Void> checkPointUpdate(String fileId, int val) {
+    private static CompletableFuture<Boolean> checkPointUpdate(final boolean batchUpdated,
+                                                               final String fileId, final int val) {
+        if (!batchUpdated) {
+            log("checkPointUpdate skipped id : " + val);
+            return noop();
+        }
+
         return CompletableFuture
-                .runAsync(() -> {
-                    sleep(2);
-                    if (INSIDE_BOOM2) throw new RuntimeException("Failed batchUpdate");
-                    log("checkPointUpdate " + val);
+                .supplyAsync(() -> {
+                    sleep(1);
+                    if (val == 4) throw new RuntimeException("Failed checkPointUpdate id : " + val);
+                    log("checkPointUpdate id : " + val);
+                    return true;
                 });
     }
 
-    private static void sleep(int seconds) {
+    private static void sleep(final int seconds) {
         try {
             Thread.sleep(TimeUnit.SECONDS.toMillis(seconds));
         } catch (InterruptedException e) {
